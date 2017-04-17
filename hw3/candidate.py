521c1d01b92a3946edda6e74a482a3b33dd1bf6a
Cindy L <TCXX@users.noreply.github.com>
2017-04-17 11:39:25 -0500

    Finished two more votes

--- hw3/candidate.py
+++ hw3/candidate.py
@@ -6,43 +6,60 @@ Created on Mon Apr 17 09:16:07 2017
 @author: XIN
 """
 
-import sys, operator
 import re #regular expression
 
 #To read and parse an input file concerning game data.
-def readFile(inputFile):
+def count(inputFile):
     #player data    
-    votes = []
+    plural = []
+    borda = []
+    approval = []
     
     #matching rules
-    valid_regex = re.compile(r"[[0-9,. ]+$")
+    valid_regex = re.compile(r"[[0-9,. \t]+$")
     number_regex = re.compile(r"\d+")
     numberEnd_regex = re.compile(r"\d+$")
     
     try:
         file = open(inputFile, 'r')
         for line in file:
-            if len(votes) == 0:
+            if len(plural) == 0:
                 number = numberEnd_regex.match(line)
                 if number is not None:
                     for i in range(0, int(line)):
-                        votes.append(0)
+                        plural.append(0)
+                        borda.append(0)
+                        approval.append(0)
                     continue
             
-            #validate format
             valid = valid_regex.match(line)
             if valid is not None:
-                #extract characters
                 matches = number_regex.findall(line)
-                time = int(matches[0])
-                #for i in range(1, len(matches)):
-                    #can = int(matches[i])
-                    #if can <= len(votes):
-                        #votes[can-1] += time
-                can = int(matches[1])
-                if can <= len(votes):
-                        votes[can-1] += 1
-        return votes
+                #time = int(matches[0])
+                
+                #validate preference range
+                isValid = True
+                for i in range(1, len(matches)):
+                    can = int(matches[i])
+                    if can > len(borda):
+                        isValid = False
+                if isValid:
+                    #plural
+                    can = int(matches[1])
+                    plural[can-1] += 1
+    
+                    #borda
+                    for i in range(1, len(matches)):
+                        can = int(matches[i])
+                        borda[can-1] += len(borda)-i
+                        
+                    #approval
+                    for i in range(1, len(matches)):
+                        can = int(matches[i])
+                        approval[can-1] += 1
+        print("Plurality vote: %s " % plural)
+        print("Borda count: %s " % borda)
+        print("Approval vote: %s " % approval)
     except IOError:
         sys.exit("IOE Error: Fail to open %s !" % inputFile)
     else:
@@ -54,5 +71,4 @@ if __name__ == "__main__":
     
     inputFile = 'ED-00016-00000001.txt'
     
-    theList=readFile(inputFile)
-    print(theList)
+    count(inputFile)
